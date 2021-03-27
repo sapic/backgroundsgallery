6bab44d9ffcc351809f3fb59a94497a0a7fd2f3b
Tim <truecarry@gmail.com>
2021-03-28 00:45:00 +0300

    Add different page name

--- src/pages/top.js
+++ src/pages/top.js
@@ -209,7 +209,7 @@ function Top() {
   return (
     <div className="bg-black">
       <Head>
-        <title>Steam.Design BG Battle</title>
+        <title>Steam.Design BG Battle Top</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="manifest" href="/manifest.webmanifest" />
