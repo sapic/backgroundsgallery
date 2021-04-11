57fd781de88685e094971764b14458c310c3e607
Tim <truecarry@gmail.com>
2021-04-11 08:15:41 +0300

    Refactor bgs scroller into it's own component, now it's not rerendered

---
+++ src/components/BackgroundsScroller.jsx
@@ -0,0 +1,86 @@
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
+import clsx from 'clsx';
+import styled from 'styled-components'
+
+const VerticalCenterDiv = styled.div`
+  top: 50%;
+  transform: translateY(-50%);
+`
+
+function ImageContainer(props) {
+  const { item, clickOnImage, ...restProps } = props
+
+  return (
+    <CSSTransition
+      key={props.item.steamUrl}
+      timeout={500}
+      classNames="item"
+      {...restProps}
+    >
+      <div className={clsx(
+        "w-full flex flex-col justify-center",
+        "absolute h-full select-none cursor-pointer",
+        // "transform scale-105 hover:scale-110 transition-all duration-500"
+      )} onClick={() => { clickOnImage(item) }}>
+        <VerticalCenterDiv className="absolute w-full">
+          <img alt="" className="w-full user-drag-none vote-container__image" src={props.item.steamUrl}></img>
+        </VerticalCenterDiv>
+        <a
+          className={clsx(
+            'bg-gray-900 text-white p-4 rounded',
+            'shadow-xl absolute bottom-12 md:bottom-48 left-1/2 w-64 md:w-128',
+            'transform', '-translate-x-1/2',
+            'transition-color duration-300 hover:bg-gray-800',
+            'hidden md:block'
+          )}
+          onClick={(e) => {
+            // e.preventDefault()
+            e.stopPropagation()
+            // console.log('click info')
+          }}
+          href={`https://steamcommunity.com/market/listings/${item.url}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="flex">
+            <div className="leading-6 align-middle">{item.name} </div>
+            <div className="ml-2 text-gray-500 text-sm leading-6 align-middle">${item.price}</div>
+          </div>
+          <div className="text-gray-500 text-sm">
+            {item.game}
+          </div>
+        </a>
+      </div>
+    </CSSTransition>
+  )
+}
+
+function BackgroundsScroller(props) {
+  const { bgs, clickOnImage } = props
+  const leftBgs = bgs.filter((_, i) => i % 2 === 0)
+  const rightBgs = bgs.filter((_, i) => i % 2 === 1)
+
+  return (
+    <>
+      <TransitionGroup
+        className="w-full h-full overflow-hidden relative vote-container"
+      >
+        {leftBgs.map((item) => (
+          <ImageContainer clickOnImage={clickOnImage} item={item} key={item.steamUrl}></ImageContainer>
+        ))}
+      </TransitionGroup>
+      <TransitionGroup className="w-full h-full overflow-hidden relative vote-container">
+        {rightBgs.map((item) => (
+          <ImageContainer clickOnImage={clickOnImage} item={item} key={item.steamUrl}></ImageContainer>
+        ))}
+      </TransitionGroup>
+    </>
+  )
+}
+
+export {
+  BackgroundsScroller
+}
--- src/pages/battle.js
+++ src/pages/battle.js
@@ -9,15 +9,9 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
 import tw from "twin.macro"
 
-import {
-  CSSTransition,
-  TransitionGroup,
-} from 'react-transition-group';
-
-// import { useIdentity } from '../lib/withIdentity'
-
 import Header from '../components/Header'
 import Tutorial from '../components/Tutorial'
+import { BackgroundsScroller } from '../components/BackgroundsScroller'
 
 // const bgs = require('../assets/bgs.json')
 
@@ -25,11 +19,6 @@ const CenterDiv = styled.div`
   top: 50%;
   left: 50%;
   transform: translateX(-50%) translateY(-50%);
-`
-
-const VerticalCenterDiv = styled.div`
-  top: 50%;
-  transform: translateY(-50%);
 `
 
 const BackgroundsContainer = styled.div`
@@ -60,36 +49,15 @@ function Home({ origin, cookies, startBgs }) {
   const [bgs, setBgs] = useState(startBgs)
   const [bgsQueue, setBgsQueue] = useState([])
 
-  // const [enableEnter, setEnableEnter] = useState(false)
-  // const { get, post, response, loading, error } = useFetch()
-
-  // console.log('home')
   async function loadBgs() {
-    // console.log('loadbgs')
-
     if (window && window.gtag) {
       window.gtag('event', 'loadbg', {
         'image': 'true'
       });
     }
 
-    // const initialTodos = await get('/api/get_random_bgs')
-    // const bgs = await fetch('https://random.bgb.workers.dev/').then(r => r.json())
     const bgs = await getNextBgs()
-    // console.log('bgs', bgs)
-
-    // let loadAwait = []
-    // for (const bg of bgs) {
-    //   loadAwait.push(preloadImage(bg.steamUrl))
-    // }
-
-    // await Promise.all(loadAwait)
-
     setBgs(bgs)
-    // if (!enableEnter) {
-    //   setTimeout(() => setEnableEnter(true), 500)
-    // }
-    // setEnableEnter(true)
   }
 
   useEffect(() => {
@@ -103,7 +71,7 @@ function Home({ origin, cookies, startBgs }) {
   async function getNextBgs() {
     if (bgsQueue.length > 0) {
       const bgs = bgsQueue.shift()
-      setBgsQueue(bgsQueue)
+      setBgsQueue([...bgsQueue])
       populateQueue()
 
       return bgs
@@ -134,7 +102,7 @@ function Home({ origin, cookies, startBgs }) {
         bgsQueue.push(bgs)
       }
 
-      setBgsQueue(bgsQueue)
+      setBgsQueue([...bgsQueue])
     }
   }
 
@@ -166,62 +134,6 @@ function Home({ origin, cookies, startBgs }) {
     loadBgs()
   }
 
-  function ImageContainer(props) {
-    const { item, ...restProps } = props
-    return (
-      <CSSTransition
-        key={props.item.steamUrl}
-        timeout={500}
-        classNames="item"
-        {...restProps}
-      >
-        <div className={clsx(
-          "w-full flex flex-col justify-center",
-          "absolute h-full select-none cursor-pointer",
-          // "transform scale-105 hover:scale-110 transition-all duration-500"
-        )} onClick={() => { clickOnImage(item) }}>
-          <VerticalCenterDiv className="absolute w-full">
-            <img alt="" className="w-full user-drag-none vote-container__image" src={props.item.steamUrl}></img>
-          </VerticalCenterDiv>
-          <a
-            className={clsx(
-              'bg-gray-900 text-white p-4 rounded',
-              'shadow-xl absolute bottom-12 md:bottom-48 left-1/2 w-64 md:w-128',
-              'transform', '-translate-x-1/2',
-              'transition-color duration-300 hover:bg-gray-800',
-              'hidden md:block'
-            )}
-            onClick={(e) => {
-              // e.preventDefault()
-              e.stopPropagation()
-              // console.log('click info')
-            }}
-            href={`https://steamcommunity.com/market/listings/${item.url}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="flex">
-              <div className="leading-6 align-middle">{item.name} </div>
-              <div className="ml-2 text-gray-500 text-sm leading-6 align-middle">${item.price}</div>
-            </div>
-            <div className="text-gray-500 text-sm">
-              {item.game}
-            </div>
-          </a>
-        </div>
-      </CSSTransition>
-    )
-  }
-
-  // const [randomBg1, randomBg2] = bgs
-  const leftBgs = bgs.filter((_, i) => i % 2 === 0)
-  const rightBgs = bgs.filter((_, i) => i % 2 === 1)
-
-  // console.log('identity', identity)
-  // const bgKeys = Object.keys(bgs)
-  // const randomBg1 = bgs[bgKeys[Math.floor(Math.random() * bgKeys.length)]]
-  // const randomBg2 = bgs[bgKeys[Math.floor(Math.random() * bgKeys.length)]]
-
   return (
     <div className="bg-black">
       <Head>
@@ -233,26 +145,10 @@ function Home({ origin, cookies, startBgs }) {
       <Header />
 
       <BackgroundsContainer>
-        <TransitionGroup
-          className="w-full h-full overflow-hidden relative vote-container"
-        >
-          {leftBgs.map((item) => (
-            <ImageContainer item={item} key={item.steamUrl}></ImageContainer>
-          ))}
-        </TransitionGroup>
-        <TransitionGroup className="w-full h-full overflow-hidden relative vote-container">
-          {rightBgs.map((item) => (
-            <ImageContainer item={item} key={item.steamUrl}></ImageContainer>
-          ))}
-        </TransitionGroup>
-        {/* <TinyCrossfade className="w-1/2 flex flex-col justify-center"> */}
-        {/* <div className="" onClick={() => { loadBgs() }} key={randomBg1.steamUrl}> */}
-        {/* <img className="w-full" src={randomBg1.steamUrl} key={randomBg1.steamUrl} ></img> */}
-        {/* </div> */}
-        {/* </TinyCrossfade> */}
-        {/* <div className="w-1/2 flex flex-col justify-center" onClick={() => { loadBgs() }}>
-            <img className="w-full" src={randomBg2.steamUrl}></img>
-          </div> */}
+        <BackgroundsScroller
+          bgs={bgs}
+          clickOnImage={clickOnImage}
+        />
 
         <CenterDiv className="absolute">
           <div className={clsx([
@@ -283,7 +179,6 @@ function Home({ origin, cookies, startBgs }) {
 }
 
 export async function getServerSideProps(ctx) {
-  // Parse
   const cookies = parseCookies(ctx)
 
   let bgs = []
@@ -301,28 +196,5 @@ export async function getServerSideProps(ctx) {
     }
   }
 }
-// Home.getInitialProps = ({ req }) => {
-//   const { origin } = absoluteUrl(req, "localhost:3000");
-
-//   return {
-//     origin,
-//   }
-// }
 
 export default Home
-
-// function absoluteUrl(req, setLocalhost) {
-//   var protocol = "https:";
-//   var host = req
-//     ? req.headers["x-forwarded-host"] || req.headers["host"]
-//     : window.location.host;
-//   if (host.indexOf("localhost") > -1) {
-//     if (setLocalhost) host = setLocalhost;
-//     protocol = "http:";
-//   }
-//   return {
-//     protocol: protocol,
-//     host: host,
-//     origin: protocol + "//" + host,
-//   };
-// }
