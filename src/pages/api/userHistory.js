dcf70d9c7c07d4c716f192b9f7d48d6cd77f0c7b
Tim <truecarry@gmail.com>
2022-05-04 06:25:59 +0300

    Fix user history

--- src/pages/api/userHistory.js
+++ src/pages/api/userHistory.js
@@ -4,13 +4,19 @@ import withPassport from '../../lib/withPassport'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const bgs = require('../../assets/bgs_full.json')
 
-export default withDatabase(withPassport(async function handler (req, res) {
+/**
+ *
+ * @param {import('next').NextApiRequest} req
+ * @param {import('next').NextApiResponse} res
+ * @returns
+ */
+const handler = async (req, res) => {
   if (!req.user || !req.user.id) {
     return res.send([])
   }
 
-  const votes = req.db('votes').where('user_id', req.user.id)
-    .sort('created_at', 'DESC')
+  const votes = await req.db('votes').where('user_id', req.user.id)
+    .orderBy('created_at', 'DESC')
     .select()
 
   // const docs = await votes.find({ user_id: req.user.id }).sort({ _id: -1 }).toArray()
@@ -18,4 +24,5 @@ export default withDatabase(withPassport(async function handler (req, res) {
   const result = votes.map(d => bgs.find(bg => bg.url === d.url)).filter(d => d)
 
   res.send(result)
-}))
+}
+export default withDatabase(withPassport(handler))
