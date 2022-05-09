a0354ac9544a7125d0888e9ecd43d67903a47297
Tim <truecarry@gmail.com>
2022-05-09 07:25:35 +0300

    Revert "Revert "Update next.config.js""
    This reverts commit 144271ccf1997f52063031e134d3a74dc41ad22e.

--- next.config.js
+++ next.config.js
@@ -1,46 +1,48 @@
 const { i18n } = require('./next-i18next.config')
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
 
 module.exports =
   // withImages({
-  withBundleAnalyzer({
-    inlineImageLimit: 1024,
+  // withBundleAnalyzer({{
+    {
+      inlineImageLimit: 1024,
 
-    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-    experimental: {
-      modern: true,
-    },
+      pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+      experimental: {
+        modern: true,
+      },
 
-    i18n,
+      i18n,
 
-    webpack (config) {
-      config.module.rules.push({
-        test: /-asset\.svg$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next',
-              name: 'static/media/[name].[hash].[ext]',
-              esModule: false,
+      webpack (config) {
+        config.module.rules.push({
+          test: /-asset\.svg$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                publicPath: '/_next',
+                name: 'static/media/[name].[hash].[ext]',
+                esModule: false,
+              },
             },
-          },
-        ],
-      })
+          ],
+        })
 
-      config.module.rules.unshift({
-        test: /\.svg$/,
-        exclude: /(node_modules)|(asset\.svg)/,
-        use: ['@svgr/webpack'],
-      })
+        config.module.rules.unshift({
+          test: /\.svg$/,
+          exclude: /(node_modules)|(asset\.svg)/,
+          use: ['@svgr/webpack'],
+        })
 
-      return config
-    },
+        return config
+      },
 
-    images: {
-      domains: ['google.com'],
-    },
-  })
+      images: {
+        domains: ['google.com'],
+      },
+    }
+// })
